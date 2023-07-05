5316e3277c5b21d35c34b767c2789b94bf283f9c
CM Antony <cmantony20@gmail.com>
2023-07-05 15:27:14 +0530

    Update tsn_ucf101.py

--- configs/recognition/tsn/tsn_ucf101.py
+++ configs/recognition/tsn/tsn_ucf101.py
@@ -13,10 +13,10 @@ model = dict(
 
 # dataset settings
 dataset_type = 'VideoDataset'
-data_root = os.path.expanduser('/content/mmaction2/MMA data/train')
-data_root_val = os.path.expanduser('/content/mmaction2/MMA data/val')
-ann_file_train = os.path.expanduser('/content/mmaction2/MMA data/train_video.txt')
-ann_file_val = os.path.expanduser('/content/mmaction2/MMA data/val_video.txt')
+data_root = os.path.expanduser('/content/MMA_2/MMA data/train')
+data_root_val = os.path.expanduser('/content/MMA_2/MMA data/val')
+ann_file_train = os.path.expanduser('/content/MMA_2/MMA data/train_video.txt')
+ann_file_val = os.path.expanduser('/content/MMA_2/MMA data/val_video.txt')
 
 file_client_args = dict(io_backend='disk')
 
@@ -139,6 +139,6 @@ default_hooks = dict(checkpoint=dict(interval=3, max_keep_ckpts=3))
 auto_scale_lr = dict(enable=False, base_batch_size=1)
 
 
-load_from = os.path.expanduser('/content/mmaction2/work_dirs/tsn_ucf101/epoch_50.pth')
+load_from = os.path.expanduser('/content/MMA_2/work_dirs/tsn_ucf101/epoch_50.pth')
 #load_from = 'https://download.openmmlab.com/mmaction/v1.0/recognition/tsn/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb/tsn_imagenet-pretrained-r50_8xb32-1x1x3-100e_kinetics400-rgb_20220906-cd10898e.pth'
 
